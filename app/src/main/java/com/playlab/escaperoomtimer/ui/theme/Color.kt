646853fbc6ed9765c6f8d3cbb1965e342adb3483
João Pedro <joaoplay16@gmail.com>
2022-11-18 19:16:01 -0300

    first commit

---
+++ app/src/main/java/com/playlab/escaperoomtimer/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.playlab.escaperoomtimer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Back900 = Color(0xFF2C2C2C)
+val Back800 = Color(0xFF323232)
+val Back700 = Color(0xFF3B3A3A)
+val Back600 = Color(0xFF4E4E4E)
+
+val Green700 = Color(0xFF2D9531)
+val Green100 = Color(0xFF00FF19)
