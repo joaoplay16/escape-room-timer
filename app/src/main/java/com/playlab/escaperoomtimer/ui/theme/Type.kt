8a3171555046000239f72fac57b55c0408133644
João Pedro <joaoplay16@gmail.com>
2022-11-18 22:34:45 -0300

    set default fonts

--- app/src/main/java/com/playlab/escaperoomtimer/ui/theme/Type.kt
+++ app/src/main/java/com/playlab/escaperoomtimer/ui/theme/Type.kt
@@ -2,27 +2,33 @@ package com.playlab.escaperoomtimer.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.playlab.escaperoomtimer.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.ds_digital_normal)),
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontSize = 96.sp,
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    h4 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.inter_regular)),
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 40.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.inter_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        color = Gray100
+    ),
+    button = TextStyle(
+        fontFamily = FontFamily(Font(R.font.inter_regular)),
+        fontWeight = FontWeight.W500,
+        fontSize = 32.sp
     )
-    */
 )
