0e789e923aab36a472414c01d41ed95d7233704e
João Pedro <joaoplay16@gmail.com>
2022-11-19 22:45:02 -0300

    create buttons components

---
+++ app/src/main/java/com/playlab/escaperoomtimer/ui/components/Buttons.kt
@@ -0,0 +1,109 @@
+package com.playlab.escaperoomtimer.ui.components
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.playlab.escaperoomtimer.R
+import com.playlab.escaperoomtimer.ui.theme.EscapeRoomTimerTheme
+
+@Composable
+fun KeypadButton(
+    modifier: Modifier = Modifier,
+    buttonText: String,
+    color: Color = MaterialTheme.colors.onSurface,
+    fontSize: TextUnit = dimensionResource(id = R.dimen.keypad_button_font_size).value.sp,
+    fontWeight: FontWeight = FontWeight.Normal,
+    textStyle: TextStyle = MaterialTheme.typography.button,
+    paddingValues: PaddingValues = PaddingValues(
+        horizontal = dimensionResource(id = R.dimen.keypad_button_horizontal_padding),
+        vertical = dimensionResource(id = R.dimen.keypad_button_vertical_padding),
+    ),
+    shape: CornerBasedShape = MaterialTheme.shapes.large,
+    onClick: (String) -> Unit = {}
+) {
+    Button(
+        modifier = modifier.clip(shape),
+        onClick = { onClick( buttonText ) }
+    ) {
+        Text(
+            modifier = Modifier.padding(paddingValues),
+            text = buttonText,
+            color = color,
+            style = textStyle,
+            textAlign = TextAlign.Center,
+            fontSize = fontSize,
+            fontWeight = fontWeight,
+            maxLines = 1
+        )
+    }
+}
+
+@Composable
+fun ActionButton(
+    modifier: Modifier = Modifier,
+    buttonText: String,
+    color: Color = MaterialTheme.colors.onSurface,
+    fontSize: TextUnit = dimensionResource(id = R.dimen.action_button_font_size).value.sp,
+    fontWeight: FontWeight = FontWeight.Normal,
+    textStyle: TextStyle = MaterialTheme.typography.button,
+    paddingValues: PaddingValues = 
+        PaddingValues(
+            horizontal = dimensionResource(id = R.dimen.action_button_horizontal_padding),
+            vertical = dimensionResource(id = R.dimen.action_button_vertical_padding)
+        ),
+    shape: CornerBasedShape = MaterialTheme.shapes.medium,
+    onClick: () -> Unit = {}
+) {
+    Button(
+        modifier = modifier.clip(shape),
+        onClick =  onClick
+    ) {
+        Text(
+            modifier = Modifier.padding(paddingValues),
+            text = buttonText,
+            color = color,
+            style = textStyle,
+            textAlign = TextAlign.Center,
+            fontSize = fontSize,
+            fontWeight = fontWeight,
+            maxLines = 1
+        )
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewActionButton() {
+    EscapeRoomTimerTheme() {
+        Surface (){
+            ActionButton(Modifier.padding(20.dp), buttonText = "START")
+        }
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewKeypadButton() {
+    EscapeRoomTimerTheme() {
+        Surface (){
+            KeypadButton(Modifier.padding(20.dp), buttonText = "1")
+        }
+    }
+}
