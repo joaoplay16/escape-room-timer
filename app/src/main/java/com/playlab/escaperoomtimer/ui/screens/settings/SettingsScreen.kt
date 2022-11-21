325961a6ae1b42b297a7a7e516137060dbfaa9ec
João Pedro <joaoplay16@gmail.com>
2022-11-21 20:41:33 -0300

    create settings screen

---
+++ app/src/main/java/com/playlab/escaperoomtimer/ui/screens/settings/SettingsScreen.kt
@@ -0,0 +1,229 @@
+package com.playlab.escaperoomtimer.ui.screens.settings
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.playlab.escaperoomtimer.R
+import com.playlab.escaperoomtimer.ui.DevicesPreviews
+import com.playlab.escaperoomtimer.ui.components.*
+import com.playlab.escaperoomtimer.ui.theme.EscapeRoomTimerTheme
+
+@Composable
+fun SettingsScreen(
+    modifier: Modifier = Modifier,
+    timerHour: String,
+    timerMinute: String,
+    timerSecond: String,
+    defuseCode: String,
+    penalty: String,
+    onArrowBackPressed: ()  -> Unit = {},
+    enableTickingSound: Boolean = true,
+    onEnableTickingSoundChange: (Boolean) -> Unit = {},
+    onPenaltyChange: (String) -> Unit = {},
+    onCodeChange: (String) -> Unit,
+    onTimerChange: (Int, Int, Int) -> Unit,
+    startCountDownTimer: () -> Unit = {},
+    countDownComposable: @Composable RowScope.() -> Unit
+) {
+    Scaffold(
+        topBar = {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colors.surface),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row(
+                    modifier = Modifier,
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Icon(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .clickable {
+                                onArrowBackPressed()
+                            },
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "BACK"
+                    )
+                    TextLabel(text = "Settings", fontSize = 36.sp)
+                }
+
+                    countDownComposable()
+            }
+        }
+    ) { paddingValues ->
+
+        val labelVerticalPadding = dimensionResource(id = R.dimen.text_label_vertical_padding)
+        val labelHorizontalPadding = dimensionResource(id = R.dimen.text_label_horizontal_padding)
+        val labelTopPadding = dimensionResource(id = R.dimen.text_label_top_padding)
+        val labelBottomPadding = dimensionResource(id = R.dimen.text_label_bottom_padding)
+
+        val orientation = LocalConfiguration.current.orientation
+
+        Row(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(20.dp)
+                    .weight(1f)
+                    .verticalScroll(rememberScrollState()),
+            ) {
+
+                var showDialog by remember{ mutableStateOf(false) }
+
+                TimePickerDialog(
+                    showDialog = showDialog,
+                    onDismissRequest = { showDialog = false},
+                    onTimeSelected = { hour, minute ->
+                        onTimerChange(hour, minute, 0)
+                    })
+
+                TextLabel( Modifier.padding(vertical =labelVerticalPadding),text = "Timer")
+                Row(verticalAlignment = Alignment.Bottom) {
+                    TimeInput(
+                        text = timerHour,
+                        readOnly = true,
+                        enabled = false,
+                        onClick = {
+                            showDialog = true
+                        }
+                    )
+                    TextLabel( Modifier.padding(horizontal = labelHorizontalPadding), text = "h")
+                    TimeInput(
+                        text = timerMinute,
+                        readOnly = true,
+                        enabled = false,
+                        onClick = {
+                            showDialog = true
+                        }
+                    )
+                    TextLabel( Modifier.padding(horizontal = labelHorizontalPadding), text = "m")
+                    TimeInput(
+                        text = timerSecond,
+                        readOnly = true,
+                        enabled = false,
+                        onClick = {
+                            showDialog = true
+                        }
+                    )
+                    TextLabel( Modifier.padding(horizontal = labelHorizontalPadding), text = "s")
+                }
+
+                TextLabel( Modifier.padding(top = labelTopPadding, bottom = labelBottomPadding), text = "Defuse code")
+                Row(verticalAlignment = Alignment.Bottom) {
+                    SecretCodeInput(
+                        text = defuseCode,
+                        onValueChange = { onCodeChange(it) }
+                    )
+                }
+
+                TextLabel( Modifier.padding(top = labelTopPadding, bottom = labelBottomPadding), text = "Penalty")
+                Row(verticalAlignment = Alignment.Bottom) {
+                    TimeInput(text = penalty, onValueChange = onPenaltyChange)
+                    TextLabel( Modifier.padding(horizontal = labelHorizontalPadding), text = "sec")
+                }
+
+                Row(
+                    Modifier.padding(
+                        top = labelTopPadding,
+                        bottom = labelHorizontalPadding
+                    ),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    TextLabel(text = "Ticking sound")
+                    SettingsCheckBox(checked = enableTickingSound, onCheckChanged = onEnableTickingSoundChange)
+                }
+
+                if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(top = 50.dp), horizontalArrangement = Arrangement.Center
+                    ) {
+                        ActionButton(buttonText = "START", onClick = startCountDownTimer)
+                    }
+                }
+            }
+
+            if (orientation == Configuration.ORIENTATION_LANDSCAPE){
+                Column(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(top = 90.dp)
+                        .weight(1f),
+
+                ) {
+                    ActionButton(buttonText = "START", onClick = startCountDownTimer)
+                }
+            }
+        }
+
+
+
+    }
+}
+
+@DevicesPreviews
+@Composable
+fun PreviewSettingsScreen() {
+    EscapeRoomTimerTheme() {
+        Surface {
+            var timerHour by remember { mutableStateOf("") }
+            var timerMinute by remember { mutableStateOf("") }
+            var timerSecond by remember { mutableStateOf("") }
+
+            SettingsScreen(
+                timerHour = timerHour,
+                timerMinute = timerMinute,
+                enableTickingSound = true,
+                onEnableTickingSoundChange = {},
+                penalty = "",
+                onPenaltyChange = {},
+                defuseCode = "",
+                onCodeChange = { },
+                timerSecond = timerSecond,
+                onTimerChange = { hour, minute, secont ->
+                    timerHour = hour.toString()
+                    timerMinute = minute.toString()
+                    timerSecond = "00"
+                },
+                countDownComposable = {
+                    Row(
+                        modifier = Modifier.padding(16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        TextLabel(
+                            modifier = Modifier.padding(end = 10.dp),
+                            text = "00:00:00"
+                        )
+                        ActionButton(
+                            buttonText = "stop",
+                            paddingValues = PaddingValues(horizontal = 10.dp, vertical = 0.dp)
+                        )
+                    }
+
+                }
+            )
+        }
+    }
+}
