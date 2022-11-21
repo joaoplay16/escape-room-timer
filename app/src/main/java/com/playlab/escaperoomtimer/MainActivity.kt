6972e4052f857e7fabbfc0c94d94b4e3eca8c414
João Pedro <joaoplay16@gmail.com>
2022-11-21 20:45:28 -0300

    setup count down timer

--- app/src/main/java/com/playlab/escaperoomtimer/MainActivity.kt
+++ app/src/main/java/com/playlab/escaperoomtimer/MainActivity.kt
@@ -1,43 +1,234 @@
 package com.playlab.escaperoomtimer
 
+import android.media.MediaPlayer
+import android.os.Build
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.playlab.escaperoomtimer.ui.components.ActionButton
+import com.playlab.escaperoomtimer.ui.components.TextLabel
+import com.playlab.escaperoomtimer.ui.screens.ScreenRoutes
+import com.playlab.escaperoomtimer.ui.screens.TimerViewModel
+import com.playlab.escaperoomtimer.ui.screens.home.HomeScreen
+import com.playlab.escaperoomtimer.ui.screens.settings.SettingsScreen
 import com.playlab.escaperoomtimer.ui.theme.EscapeRoomTimerTheme
+import com.playlab.escaperoomtimer.util.TimeUtil.getLeftPaddedNumberString
+import com.playlab.escaperoomtimer.util.TimeUtil.getTimeInMillis
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.math.floor
 
 class MainActivity : ComponentActivity() {
+
+    var timer: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             EscapeRoomTimerTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
+                    DefaultNavHost(
+
+                    )
                 }
+            }
+        }
+
+    }
+
+    private fun startTimer(timerViewModel: TimerViewModel){
+
+        val timeUntilFinishInMillis by timerViewModel.timeUntilFinishInMillis
+        val tickSoundEnabled by timerViewModel.tickSoundEnabled
+
+        timer = object: CountDownTimer(timeUntilFinishInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+
+                if (tickSoundEnabled) playSound(R.raw.beep)
+
+                val timerHour = floor((millisUntilFinished.toDouble() / (1000 * 60 * 60)) % 24).toLong()
+                val timerMinute = floor((millisUntilFinished.toDouble() / (1000 * 60)) % 60).toLong()
+                val timerSecond = floor((millisUntilFinished.toDouble() / 1000) % 60).toLong()
+
+                timerViewModel.setTimeHour(timerHour)
+                timerViewModel.setTimeMinute(timerMinute)
+                timerViewModel.setTimeSecond(timerSecond)
+
+                timerViewModel.setTimeUntilFinishInMillis(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                timerViewModel.resetTimer()
+                playSound(R.raw.bomb_explosion)
+                cancel()
+            }
+        }.start()
+    }
+
+    private fun stopTimer(){
+        timer?.cancel()
+        timer = null
+    }
+
+    fun playSound(resId: Int){
+        CoroutineScope(Dispatchers.IO).launch {
+            val mp = MediaPlayer()
+
+            try{
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    mp.setDataSource(this@MainActivity.resources.openRawResourceFd(resId))
+                }
+                mp.prepare()
+                mp.start()
+            }catch (e: Exception){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Composable
+    fun DefaultNavHost(
+        modifier: Modifier = Modifier,
+        navController: NavHostController = rememberNavController(),
+        startDestination: String = ScreenRoutes.Home.name,
+        timerViewModel: TimerViewModel = viewModel()
+    ) {
+
+
+        val defuseCode by timerViewModel.defuseCode
+        val tickSoundEnabled by timerViewModel.tickSoundEnabled
+        val timeString = timerViewModel.getTimeString()
+        val timeUntilFinish by timerViewModel.timeUntilFinishInMillis
+        val penalty by timerViewModel.penalty
+
+        val code by timerViewModel.code
+
+        NavHost(
+            modifier = modifier,
+            navController = navController,
+            startDestination = startDestination,
+        ){
+            composable(ScreenRoutes.Home.name){
+                HomeScreen(
+                    timerText = timeString,
+                    timeUntilFinish = timeUntilFinish,
+                    code = code,
+                    onCodeChange = { digit ->
+                        val c = StringBuilder(code).append(digit).toString()
+                        timerViewModel.setCode(c)
+                                   },
+                    onKeypadDelete = {
+                        timerViewModel.setCode(code.dropLast(1))
+                    },
+                    onKeypadOk = {
+                        val hasDefused = timerViewModel.hasDefused()
+
+                        val c = timerViewModel.code.value
+                        val dc = timerViewModel.defuseCode.value
+
+                        Log.d("CODE", "code $c defuseCode $dc" )
+
+                        if(timeUntilFinish > 0){
+                            if(hasDefused){
+                                playSound(R.raw.bomb_has_been_defused)
+                                timerViewModel.resetTimer()
+                                stopTimer()
+                            }else{
+                                playSound(R.raw.error)
+                                stopTimer()
+                                timerViewModel.setCode("")
+                                timerViewModel.setTimeUntilFinishInMillis(timeUntilFinish - (penalty.toInt() * 1000L))
+                                startTimer(timerViewModel)
+                            }
+                        }
+
+                    },
+                    onSettingsClick = {
+                        navController.navigate(ScreenRoutes.Settings.name)
+                    },
+                )
+            }
+
+            composable(ScreenRoutes.Settings.name){
+                var textTimerHour by remember { mutableStateOf("") }
+                var textTimerMinute by remember { mutableStateOf("") }
+                var textTimerSecond by remember { mutableStateOf("") }
+
+
+                SettingsScreen(
+                    timerHour = getLeftPaddedNumberString(textTimerHour.ifEmpty { "0" }.toInt()),
+                    timerMinute = getLeftPaddedNumberString(textTimerMinute.ifEmpty { "0" }.toInt()),
+                    timerSecond = getLeftPaddedNumberString((textTimerSecond).ifEmpty { "0" }.toInt()),
+                    enableTickingSound = tickSoundEnabled,
+                    onEnableTickingSoundChange = { timerViewModel.setEnabledTickSound(it) },
+                    penalty = penalty,
+                    onPenaltyChange = {
+                        timerViewModel.setPenaltyValue(it)
+                    },
+                    defuseCode = defuseCode,
+                    onCodeChange = {
+
+                        timerViewModel.setDefuseCode(it)
+                                   },
+                    onTimerChange = { hour, minute, second ->
+                        textTimerHour = hour.toString()
+                        textTimerMinute = minute.toString()
+                        textTimerSecond = "00"
+
+                        timerViewModel.setStartTimeInMillis(getTimeInMillis(hour, minute, second))
+                    },
+                    startCountDownTimer = {
+                        stopTimer()
+                        if(timeUntilFinish > 0) startTimer(timerViewModel)
+                        navController.popBackStack()
+                    },
+                    onArrowBackPressed = { navController.popBackStack() },
+                    countDownComposable = {
+
+                        Row(
+                            modifier = Modifier.padding(16.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            TextLabel(
+                                modifier = Modifier.padding(end = 10.dp),
+                                text = timeString
+                            )
+                            ActionButton(
+                                buttonText = "stop",
+                                onClick = {
+                                    timerViewModel.resetTimer()
+                                    stopTimer()
+                                },
+                                paddingValues = PaddingValues(horizontal = 10.dp, vertical = 0.dp)
+                            )
+                        }
+                    }
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    EscapeRoomTimerTheme {
-        Greeting("Android")
-    }
-}
+
