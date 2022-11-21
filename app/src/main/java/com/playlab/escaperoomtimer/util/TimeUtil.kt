0ffacedc9f6023787b57b22a4370276b15bb048d
João Pedro <joaoplay16@gmail.com>
2022-11-21 20:37:27 -0300

    add new time parsing functions

--- app/src/main/java/com/playlab/escaperoomtimer/util/TimeUtil.kt
+++ app/src/main/java/com/playlab/escaperoomtimer/util/TimeUtil.kt
@@ -1,13 +1,22 @@
 package com.playlab.escaperoomtimer.util
 
 import java.util.*
+import kotlin.math.floor
 
 object TimeUtil {
     fun getFormattedTimeString(hour: Int = 0, minute: Int = 0, second: Int = 0): String {
         val time = String.format(
             Locale.getDefault(),
             "%02d:%02d:%02d",
-            hour, minute, second);
+            hour, minute, second)
+        return time
+    }
+
+    fun getLeftPaddedNumberString(number: Int = 0): String {
+        val time = String.format(
+            Locale.getDefault(),
+            "%02d",
+            number)
         return time
     }
 
@@ -21,5 +30,13 @@ object TimeUtil {
         val secondsInMillis = second * ONE_SECOND
 
         return  hourInMillis + minutesInMillis + secondsInMillis
+    }
+
+    fun getFormattedTimeString(millis: Long): String{
+        val hour = floor((millis.toDouble() / (1000 * 60 * 60)) % 24).toLong()
+        val minute = floor((millis.toDouble() / (1000 * 60)) % 60).toLong()
+        val second = floor((millis.toDouble() / 1000) % 60).toLong()
+
+        return getFormattedTimeString(hour.toInt(), minute.toInt(), second.toInt())
     }
 }
