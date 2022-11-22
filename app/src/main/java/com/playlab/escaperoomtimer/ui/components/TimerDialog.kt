1acf13a5c4adb9bdaac5dc28246bc043fe1dfb21
João Pedro <joaoplay16@gmail.com>
2022-11-22 00:48:09 -0300

    create dialog component

---
+++ app/src/main/java/com/playlab/escaperoomtimer/ui/components/TimerDialog.kt
@@ -0,0 +1,66 @@
+package com.playlab.escaperoomtimer.ui.components
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.playlab.escaperoomtimer.ui.theme.EscapeRoomTimerTheme
+
+@Composable
+fun TimerDialog(
+    modifier: Modifier = Modifier,
+    title: String = "",
+    onOkClick: () -> Unit = {},
+    onCancelClick: () -> Unit = {},
+    onDismiss: () -> Unit = {},
+
+) {
+    AlertDialog(
+        modifier = modifier,
+        title = { Text(text = title) },
+        buttons = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.End
+            ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(top = 10.dp, bottom = 15.dp, start = 10.dp, end = 15.dp)
+                            .clickable { onCancelClick() },
+                        text = "No",
+                        color = MaterialTheme.colors.onSurface,
+                        style = MaterialTheme.typography.overline
+                    )
+                    Text(
+                        modifier = Modifier
+                            .padding(top = 10.dp, bottom = 15.dp, start = 10.dp, end = 20.dp)
+                            .clickable { onOkClick()  },
+                        text = "Yes",
+                        color = MaterialTheme.colors.onSurface,
+                        style = MaterialTheme.typography.overline
+                    )
+            }
+
+        },
+        onDismissRequest = onDismiss,
+    )
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewTimerDialog() {
+
+    EscapeRoomTimerTheme() {
+        Surface {
+            TimerDialog(modifier = Modifier.fillMaxWidth(0.8f))
+        }
+    }
+}
