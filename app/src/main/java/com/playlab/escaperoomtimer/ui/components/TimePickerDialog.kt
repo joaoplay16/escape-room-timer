af10c55af89d4d7cc784b8d9723cfd6dc7d3435e
João Pedro <joaoplay16@gmail.com>
2022-11-23 13:07:41 -0300

    set up initial hour and minute

--- app/src/main/java/com/playlab/escaperoomtimer/ui/components/TimePickerDialog.kt
+++ app/src/main/java/com/playlab/escaperoomtimer/ui/components/TimePickerDialog.kt
@@ -1,5 +1,6 @@
 package com.playlab.escaperoomtimer.ui.components
 
+import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
 import android.widget.TimePicker
@@ -7,8 +8,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -21,9 +23,10 @@ fun TimePickerDialog(
     modifier: Modifier = Modifier,
     showDialog: Boolean = false,
     onDismissRequest: () -> Unit = {},
-    onTimeSelected: (Int, Int) -> Unit
+    onTimeSelected: (Int, Int) -> Unit,
+    initialHour: Int = 0,
+    initialMinute: Int = 0,
 ) {
-
 
     if (showDialog) {
         Dialog(
@@ -43,7 +46,14 @@ fun TimePickerDialog(
                             .inflate(R.layout.timer_picker_dialog, null, false)
                                 as TimePicker
 
-                        timePickerLayout.setIs24HourView(true)
+                       timePickerLayout.setIs24HourView(true)
+                       if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                           timePickerLayout.hour = initialHour
+                           timePickerLayout.minute = initialMinute
+                       }else{
+                           timePickerLayout.currentHour = initialHour
+                           timePickerLayout.currentMinute = initialMinute
+                       }
 
                        timePickerLayout.setOnTimeChangedListener { _, hour, minute ->
                           onTimeSelected(hour, minute)
